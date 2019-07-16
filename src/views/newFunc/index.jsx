a09efb518ca68b06fe9102a830eb34fc57eeeafe
wang kai <kaikai.wang@m2motive.com.cn>
2019-07-16 16:10:01 +0800

    分组全选功能

--- src/views/newFunc/index.jsx
+++ src/views/newFunc/index.jsx
@@ -2,19 +2,29 @@ import React, { Component } from "react";
 import { Checkbox, Row, Col, AutoComplete } from 'antd';
 
 const plainOptions = ['Apple', 'Pear', 'Orange'];
-const defaultCheckedList = ['Apple', 'Orange'];
+const plainOptions1 = ['A', 'B', 'C'];
 
 class newFunc extends Component {
     state = {
-        checkedList: defaultCheckedList,
+        checkedList: ['Apple', 'Orange'],
+        checkedList1: ['A'],
         indeterminate: true,
+        indeterminate1: true,
         checkAll: false,
+        checkAll1: false,
     };
     onChange = checkedList => {
         this.setState({
             checkedList,
             indeterminate: !!checkedList.length && checkedList.length < plainOptions.length,
             checkAll: checkedList.length === plainOptions.length,
+        });
+    };
+    onChange1 = checkedList1 => {
+        this.setState({
+            checkedList1,
+            indeterminate1: !!checkedList1.length && checkedList1.length < plainOptions1.length,
+            checkAll1: checkedList1.length === plainOptions1.length,
         });
     };
 
@@ -25,32 +35,81 @@ class newFunc extends Component {
             checkAll: e.target.checked,
         });
     };
+    onCheckAllChange1 = e => {
+        this.setState({
+            checkedList1: e.target.checked ? plainOptions1 : [],
+            indeterminate1: false,
+            checkAll1: e.target.checked,
+        });
+    };
+    onCheckAllChange3 = e => {
+        this.setState({
+            checkedList: e.target.checked ? plainOptions : [],
+            checkedList1: e.target.checked ? plainOptions1 : [],
+            indeterminate: false,
+            indeterminate1: false,
+            checkAll: e.target.checked,
+            checkAll1: e.target.checked,
+        });
+    };
     render() {
         return (
             <div>
                 <div style={{ borderBottom: '1px solid #E9E9E9' }}>
                     <Checkbox
-                        indeterminate={this.state.indeterminate}
-                        onChange={this.onCheckAllChange}
-                        checked={this.state.checkAll}
+                        indeterminate={this.state.indeterminate || this.state.indeterminate1 || (this.state.checkAll || this.state.checkAll1) && !(this.state.checkAll && this.state.checkAll1)}
+                        onChange={this.onCheckAllChange3}
+                        checked={this.state.checkAll && this.state.checkAll1}
                     >
-                        Check all
+                        Check all all
                     </Checkbox>
                 </div>
                 <br />
-                <Checkbox.Group style={{ width: '50%' }} onChange={this.onChange} value={this.state.checkedList}>
-                    <Row>{
-                        plainOptions.map(el => {
-                            return (
-                                <Col span={8}>
-                                    <Checkbox value={el}>{el}</Checkbox>
-                                </Col>
-                            )
-                        })
-                    }
-                    </Row>
-                </Checkbox.Group>
-            </div>
+                <div style={{ display: 'flex' }}>
+                    <div style={{ borderBottom: '1px solid #E9E9E9', width: '40%' }}>
+                        <Checkbox
+                            indeterminate={this.state.indeterminate}
+                            onChange={this.onCheckAllChange}
+                            checked={this.state.checkAll}
+                        >
+                            Check all
+                    </Checkbox>
+                        <Checkbox.Group onChange={this.onChange} value={this.state.checkedList}>
+                            <Row>{
+                                plainOptions.map(el => {
+                                    return (
+                                        <Col key={el}>
+                                            <Checkbox value={el}>{el}</Checkbox>
+                                        </Col>
+                                    )
+                                })
+                            }
+                            </Row>
+                        </Checkbox.Group>
+                    </div>
+                    <div style={{ borderBottom: '1px solid #E9E9E9' }}>
+                        <Checkbox
+                            indeterminate={this.state.indeterminate1}
+                            onChange={this.onCheckAllChange1}
+                            checked={this.state.checkAll1}
+                        >
+                            Check all
+                    </Checkbox>
+                        <Checkbox.Group onChange={this.onChange1} value={this.state.checkedList1}>
+                            <Row>{
+                                plainOptions1.map(el => {
+                                    return (
+                                        <Col key={el}>
+                                            <Checkbox value={el}>{el}</Checkbox>
+                                        </Col>
+                                    )
+                                })
+                            }
+                            </Row>
+                        </Checkbox.Group>
+                    </div>
+                </div>
+            </div >
         )
     }
 }
